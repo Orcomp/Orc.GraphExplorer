e54557ca9195b267966595133855c90ca11c64cc
Geert van Horrik <geert@catenalogic.com>
2014-12-01 17:44:19 +0100

    Improved usage of Orchestra

--- src/Orc.GraphExplorer.Demo/Views/MainWindow.xaml.cs
+++ src/Orc.GraphExplorer.Demo/Views/MainWindow.xaml.cs
@@ -1,19 +1,22 @@
-﻿namespace Orc.GraphExplorer.Demo.Views
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Orcomp development team">
+//   Copyright (c) 2008 - 2014 Orcomp development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.GraphExplorer.Demo.Views
 {
-    using System.Windows;
-    using System.Windows.Controls;
-
-    using Orchestra.Services;
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        #region Constructors
         public MainWindow()
         {
             InitializeComponent();
-            
         }
+        #endregion
     }
 }
